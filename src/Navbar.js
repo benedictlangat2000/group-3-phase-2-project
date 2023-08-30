888e301181547d573daba88fc9533c2f09b250a9
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-08-30 21:59:37 +0300

    add style

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,10 +5,29 @@ import Nav from 'react-bootstrap/Nav';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import { FaUser, FaShoppingCart } from 'react-icons/fa';
+import { Outlet, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 
 function EcommerceNavbar() {
-  return (
+
+  const[products, setProducts] = useState([])
+    
+   useEffect(()=>{
+            fetch("http://ecommerce.muersolutions.com/api/v1/products")
+            .then(response=>response.json())
+            .then(data=>setProducts(data))
+            .catch(error=>console.log(error))
+           
+
+        }, [])
+        // console.log(products)
+    
+
+  return (<div>
+    <nav>
+     
+      
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="/">Group-3 shopify</Navbar.Brand>
@@ -21,11 +40,48 @@ function EcommerceNavbar() {
           <Nav>
             <Nav.Link href="/account"><FaUser /> Account</Nav.Link>
             <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+            
           </Nav>
+         
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+    <nav className='nav'>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/signup">Signup</Link>
+      </li>
+      <li>
+        <Link to="/cart">Cart</Link>
+      </li>
+    </ul>
+  </nav>
+  </nav>
+  <Outlet></Outlet>
+  <div>
+  <h1>Here are the products available currently</h1>
+  <p>We are glad to serve you!</p>
+  
+{products.map((product,index)=>(
+          <div key={index}> 
+              <ul>
+                  <li>
+                 
+                  <h2>{product.product_name} </h2>
+                  <img src ={product.product_full_image} alt={product.product_thumbnail}/>
+                  <button><Link to ={`/${index}`}>View Details</Link></button>
+                  </li>
+              </ul>
+          </div>
+))}
+
+</div>
+
+    
+</div>);
 }
 
 export default EcommerceNavbar;
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,49 +1,35 @@
-import React from "react";
-import { useState, useEffect } from "react"
-import {Link} from "react-router-dom";
+// import React from "react";
+// import { useState, useEffect } from "react"
+// import {Link} from "react-router-dom";
+// import EcommerceNavbar from "./Navbar";
 
 
 
-function ProductList(){
+// function ProductList(){
+    // const[products, setProducts] = useState([])
     
-const[products, setProducts] = useState([])
-    
-    try {
-        useEffect(()=>{
-            fetch("http://ecommerce.muersolutions.com/api/v1/products")
-            .then(response=>response.json())
-            .then(data=>setProducts(data))
+//    useEffect(()=>{
+//             fetch("http://ecommerce.muersolutions.com/api/v1/products")
+//             .then(response=>response.json())
+//             .then(data=>console.log(data))
+//             .catch(error=>console.log(error))
+           
 
-        }, [])
+//         }, [])
+//         // console.log(products)
         
-    } catch (error) {
-        console.log(error)
-        
-        
-    }
-    console.log(products)
+
+   
 
     
-    return( <div>
-            <h1>Here are the products available currently</h1>
-            <p>We are glad to serve you!</p>
-            
-    {products.map((product,index)=>(
-                    <div key={index}> 
-                        <ul>
-                            <li>
-                           
-                            <h2>{product.product_name} </h2>
-                            <img src ={product.product_full_image} alt={product.product_thumbnail}/>
-                            <button><Link to ={`/${index}`}>View Details</Link></button>
-                            </li>
-                        </ul>
-                    </div>
-    ))}
-    
-        </div>)
+//     return( <div>
+        
+//         <EcommerceNavbar products={products}/>
+//         </div>
+        
+//     )
 
     
             
-}
-export default ProductList
+// // }
+// export default ProductList
