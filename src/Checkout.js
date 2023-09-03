6516a5c366914dccbdcdb4081d1576f6cdd67ae8
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-09-03 23:21:24 +0300

    add components Checkout AddressandLocationForm OrderReceit PaymentForm

---
+++ src/Checkout.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import AddressAndLocationForm from "./AddressAndLocationForm";
+
+import PaymentForm from "./PaymentForm";
+import OrderReceipt from "./OrderReceipt";
+
+function Checkout({ cartItems }) {
+  const [step, setStep] = useState(1); 
+  const [formData, setFormData] = useState({
+    shippingAddress: {},
+    paymentInfo: {},
+  });
+
+  const handleAddressSubmit = (addressData) => {
+    setFormData({ ...formData, shippingAddress: addressData });
+    setStep(2); // Move to the payment step
+  };
+
+  const handlePaymentSubmit = (paymentData) => {
+    setFormData({ ...formData, paymentInfo: paymentData });
+   
+    const totalAmount = calculateTotalAmount(cartItems); 
+    const order = {
+      shippingAddress: formData.shippingAddress,
+      paymentInfo: formData.paymentInfo,
+      totalAmount,
+     
+    };
+    setFormData({ ...formData, order });
+    setStep(3); // Move to the receipt step
+  };
+
+  const calculateTotalAmount = (items) => {
+    // Calculate the total amount based on cart items
+    return items.reduce((total, item) => total + item.unit_price, 0);
+  };
+
+  const handleStepBack = () => {
+    setStep(step - 1);
+  };
+
+  const handleStepNext = () => {
+    setStep(step + 1);
+  };
+
+  return (
+    <div>
+      {step === 1 && (
+        <AddressAndLocationForm onNext={handleAddressSubmit} />
+      )}
+      {step === 2 && (
+        <PaymentForm onNext={handlePaymentSubmit} />
+      )}
+      {step === 3 && (
+        <OrderReceipt order={formData.order} />
+      )}
+      <div>
+        {step > 1 && (
+          <button onClick={handleStepBack}>Back</button>
+        )}
+        {step < 3 && (
+          <button onClick={handleStepNext}>Next</button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
---
+++ src/OrderReceipt.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function OrderReceipt({ order }) {
+  return (
+    <div>
+      <h2>Order Receipt</h2>
+      {order && (
+        <div>
+         
+          <p>Shipping Address:</p>
+          <p>Name: {order.shippingAddress.name}</p>
+          <p>Address Line: {order.shippingAddress.addressLine}</p>
+          <p>City: {order.shippingAddress.city}</p>
+          <p>Postal Code: {order.shippingAddress.postalCode}</p>
+          <p>Selected County: {order.shippingAddress.selectedCounty}</p>
+          <p>Total Amount: ${order.totalAmount}</p>
+          
+        </div>
+      )}
+      <button onClick={() => window.print()}>Print Receipt</button>
+    </div>
+  );
+}
+
+export default OrderReceipt;
